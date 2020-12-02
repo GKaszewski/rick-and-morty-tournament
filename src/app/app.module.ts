6d456b08ba58b66573f4221909f4e121965da81d
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-12-02 17:01:20 +0100

    HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
@@ -11,7 +12,8 @@ import { ApiService } from './services/api.service';
     CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
