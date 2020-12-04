feaed731930a99a7c91fc9e4e1a203a39fdb1517
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-12-04 23:15:22 +0100

    Loader and Interceptor service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ApiService } from './services/api.service';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { TableComponent } from './table/table.component';
+import { TableElementComponent } from './table-element/table-element.component';
+import { LoaderService } from './services/loader.service';
+import { InterceptorService } from './services/interceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    SpinnerComponent,
+    TableComponent,
+    TableElementComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [ApiService],
+  providers: [ApiService, LoaderService, { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/loader.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class LoaderService {
+    public isLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+}
